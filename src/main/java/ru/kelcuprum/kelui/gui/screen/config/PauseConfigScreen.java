2db9286790aef2736641138b7333acdc63ff1735
Kel <me@kelcuprum.ru>
2024-06-02 19:28:16 +0300

    Niko roomba

--- src/main/java/ru/kelcuprum/kelui/gui/screen/config/PauseConfigScreen.java
+++ src/main/java/ru/kelcuprum/kelui/gui/screen/config/PauseConfigScreen.java
@@ -40,7 +40,7 @@ public class PauseConfigScreen {
                         .addValue(new EditBoxBuilder(Component.translatable("kelui.config.pause_menu.short_command.command")).setValue("/lobby").setConfig(KelUI.config, "PAUSE_MENU.SHORT_COMMAND.COMMAND").build())
                 )
                 .addWidget(new CategoryBox(Component.translatable("kelui.config.pause_menu.oneshot"))
-                        .addValue(new ButtonBooleanBuilder(Component.translatable("kelui.config.pause_menu.niko_roomba"), true).setConfig(KelUI.config, "PAUSE_MENU.NIKO_ROOMBA").build())
+                        .addValue(new ButtonBooleanBuilder(Component.translatable("kelui.config.pause_menu.niko_roomba"), false).setConfig(KelUI.config, "PAUSE_MENU.NIKO_ROOMBA").build())
                 )
                 .build();
     }
