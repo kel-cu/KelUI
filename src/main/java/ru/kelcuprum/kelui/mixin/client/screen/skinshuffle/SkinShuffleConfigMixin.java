dd07fbdb1dc9a8357c432890ce1a748f2b7df4c0
Kel <me@kelcuprum.ru>
2024-07-12 19:56:15 +0300

    yeah

--- src/main/java/ru/kelcuprum/kelui/mixin/client/screen/skinshuffle/SkinShuffleConfigMixin.java
+++ src/main/java/ru/kelcuprum/kelui/mixin/client/screen/skinshuffle/SkinShuffleConfigMixin.java
@@ -1,6 +1,7 @@
 package ru.kelcuprum.kelui.mixin.client.screen.skinshuffle;
 
 import com.mineblock11.skinshuffle.client.gui.widgets.OpenCarouselButton;
+import com.mineblock11.skinshuffle.client.gui.widgets.WarningIndicatorButton;
 import net.minecraft.client.gui.components.Renderable;
 import net.minecraft.client.gui.components.events.GuiEventListener;
 import net.minecraft.client.gui.narration.NarratableEntry;
@@ -11,6 +12,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import ru.kelcuprum.alinlib.AlinLib;
 import ru.kelcuprum.kelui.KelUI;
 
 @Mixin(Screen.class)
@@ -18,10 +20,11 @@ public class SkinShuffleConfigMixin {
     @Inject(method = "addRenderableWidget", at = @At("HEAD"), cancellable = true)
     private <T extends GuiEventListener & Renderable & NarratableEntry>void addRenderableWidget(T guiEventListener, CallbackInfoReturnable<T> cir){
         if(((Screen) (Object) this instanceof TitleScreen && KelUI.config.getBoolean("MAIN_MENU", true)) || ((Screen) (Object) this instanceof PauseScreen  && KelUI.config.getBoolean("PAUSE_MENU", true))){
-            if(guiEventListener instanceof OpenCarouselButton) {
+            if(guiEventListener instanceof OpenCarouselButton || guiEventListener instanceof WarningIndicatorButton) {
                 cir.setReturnValue(guiEventListener);
                 cir.cancel();
             }
+            AlinLib.log(guiEventListener.getClass().getName());
         }
     }
 }
