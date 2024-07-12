6555d1159363f2e34a13c4f596eea15d605c5638
Kel <me@kelcuprum.ru>
2024-07-12 19:28:08 +0300

    Cleanup

--- src/main/java/ru/kelcuprum/kelui/gui/components/OneShotButton.java
+++ src/main/java/ru/kelcuprum/kelui/gui/components/OneShotButton.java
@@ -10,7 +10,6 @@ import net.minecraft.client.sounds.SoundManager;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
-import net.minecraft.sounds.SoundEvents;
 import net.minecraft.util.Mth;
 
 public class OneShotButton extends AbstractButton {
--- src/main/java/ru/kelcuprum/kelui/gui/components/SSButtons.java
+++ src/main/java/ru/kelcuprum/kelui/gui/components/SSButtons.java
@@ -1,13 +1,8 @@
 package ru.kelcuprum.kelui.gui.components;
 
-import com.mineblock11.skinshuffle.client.gui.CarouselScreen;
 import com.mineblock11.skinshuffle.client.gui.GeneratedScreens;
 import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.network.chat.Component;
 import ru.kelcuprum.alinlib.AlinLib;
-import ru.kelcuprum.alinlib.gui.components.builder.button.ButtonBuilder;
-
-import static ru.kelcuprum.kelui.KelUI.ICONS.CAPES;
 
 public class SSButtons {
     public static Screen getScreen(){
