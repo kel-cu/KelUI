326a9fe07d69ea50d3eb663a514c9b6faf381de2
Kel <me@kelcuprum.ru>
2024-10-28 21:37:29 +0300

    .

--- src/main/java/ru/kelcuprum/kelui/mixin/client/utils/GuiMixin.java
+++ src/main/java/ru/kelcuprum/kelui/mixin/client/utils/GuiMixin.java
@@ -29,7 +29,6 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import ru.kelcuprum.alinlib.AlinLib;
 import ru.kelcuprum.kelui.KelUI;
